9e8da16cf2e3ff402cc6de6645cf17d8fc845a42
pys-durup <enenfnq@gmail.com>
2021-05-21 17:43:13 +0900

    securityformlogin 프로젝트 생성

---
+++ securityformlogin/src/test/java/com/spring/securityformlogin/SecurityformloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.securityformlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityformloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
