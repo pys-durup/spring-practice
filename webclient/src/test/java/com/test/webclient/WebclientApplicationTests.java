25e73a1bf2a222c2e652af0dcf189eecd4e6fa3c
pys-durup <enenfnq@gmail.com>
2021-12-16 15:22:13 +0900

    webclient init

---
+++ webclient/src/test/java/com/test/webclient/WebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
